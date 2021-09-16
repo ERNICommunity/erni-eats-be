421c58ae6b1ddc9933ca4b651cc87d3b922d2150
toma-s <toma.savkova@gmail.com>
2021-09-16 15:57:58 +0200

    Move contact info to establishment model

--- src/main/java/erni/betterask/eats/be/service/EstablishmentContactInfoService.java
+++
@@ -1,9 +0,0 @@
-package erni.betterask.eats.be.service;
-
-import erni.betterask.eats.be.model.ContactInfo;
-
-import java.util.Optional;
-
-public interface EstablishmentContactInfoService {
-    Optional<ContactInfo> findByEstablishmentId(String establishmentId);
-}
